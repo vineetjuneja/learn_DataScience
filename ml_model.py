c9e3c35c66284e802c4497b30320eedcb8490104
vineetjuneja <askvineetjuneja@gmail.com>
2023-06-22 00:23:41 +0530

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print ('adding all ML related code here')
+
+def model1(x,y):
+    print ('adding code for model 1')
+
+def model2(x,y):
+    print ('adding code for model 2')
+
+def model3(x,y):
+    print ('adding code for model 3')
